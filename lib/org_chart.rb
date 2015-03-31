998dbc4b819af0b7d89869d27e9f01750ee4dede
iexus <2.-@twopointline.com>
2015-03-31 22:43:12 +0100

    Accidental and

--- lib/org_chart.rb
+++ lib/org_chart.rb
@@ -12,7 +12,7 @@ class OrgChart
 
     start_employee = org_hierarchy.employees_with_name(start_employee_name).first
     end_employee = org_hierarchy.employees_with_name(end_employee_name).first
-    raise OrgChartError.new("Could not find an employee in organisation: #{start_employee_name}, #{end_employee_name}") if start_employee.nil? && end_employee.nil?
+    raise OrgChartError.new("Could not find an employee in organisation: #{start_employee_name}, #{end_employee_name}") if start_employee.nil? || end_employee.nil?
 
     route = RoutePlanner.new(org_hierarchy).calculate_route(start_employee, end_employee)
     RoutePrinter.new.print_route(route)
